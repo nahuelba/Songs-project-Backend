bd337c161182497bab23630956751c158ba0028c
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-04-24 14:22:55 -0300

    Update urls.py

--- songoftheday/api/urls.py
+++ songoftheday/api/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .api import InterpreterListAPIView,  LinksSongsListAPIView, SongListAPIView, SongofthedayListAPIView
+from .api import SongofthedayListAPIView
 
 
 urlpatterns = [
-    path("song/", SongListAPIView.as_view(), name="song_api"),
-    path("song/links/", LinksSongsListAPIView.as_view(), name="links_api"),
-    path("song/links/interpreter/", InterpreterListAPIView.as_view(), name="interpreter_api"),
     path("songoftheday/", SongofthedayListAPIView.as_view(), name="songoftheday_api")
-
-    
 
 ]
