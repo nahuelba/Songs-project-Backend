c8583117ddfd536ccd4d9f91d2a3f8441a69e9c5
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-04-24 14:22:08 -0300

    Update api.py

--- songoftheday/api/api.py
+++ songoftheday/api/api.py
@@ -1,35 +1,11 @@
 from rest_framework.generics import ListAPIView
 from songoftheday.models import InterpreterModel, LinksSongsModel, SongModel
-from songoftheday.api.serializers import interpreterSerializer, LinkSongsSerializer, SongSerializer
+from songoftheday.api.serializers import SongSerializer
 from datetime import datetime
 
 
 
 
-class InterpreterListAPIView(ListAPIView):
-
-    serializer_class = interpreterSerializer
-
-    def get_queryset(self):
-        return InterpreterModel.objects.all()
-
-
-
-class LinksSongsListAPIView(ListAPIView):
-
-    serializer_class = LinkSongsSerializer
-
-    def get_queryset(self):
-        return LinksSongsModel.objects.all()
-
-
-
-class SongListAPIView(ListAPIView):
-
-    serializer_class = SongSerializer
-
-    def get_queryset(self):
-        return SongModel.objects.all()
 
 class SongofthedayListAPIView(ListAPIView):
 
